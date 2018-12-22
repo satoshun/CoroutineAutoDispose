4af30cab67b7cf8004de32af9382e22fe26b074c
SatoShun <shun.sato1@gmail.com>
2018-12-22 04:40:02 +0000

    sample use LifecycleObserver and call method from client side

--- app/src/main/java/com/github/satoshun/example/sample/BaseActivity.kt
+++ app/src/main/java/com/github/satoshun/example/sample/BaseActivity.kt
@@ -1,6 +1,7 @@
 package com.github.satoshun.example.sample
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -15,5 +16,16 @@ abstract class BaseActivity : AppCompatActivity(),
   override fun onDestroy() {
     super.onDestroy()
     job.cancel()
+  }
+}
+
+fun BaseActivity.addJob(job: Job) {
+  val state = lifecycle.currentState
+  when (state) {
+    Lifecycle.State.DESTROYED -> TODO()
+    Lifecycle.State.INITIALIZED -> lifecycle.addObserver(AnyObserver(job, Lifecycle.Event.ON_DESTROY))
+    Lifecycle.State.CREATED -> lifecycle.addObserver(AnyObserver(job, Lifecycle.Event.ON_DESTROY))
+    Lifecycle.State.STARTED -> lifecycle.addObserver(AnyObserver(job, Lifecycle.Event.ON_STOP))
+    Lifecycle.State.RESUMED -> lifecycle.addObserver(AnyObserver(job, Lifecycle.Event.ON_PAUSE))
   }
 }
--- app/src/main/java/com/github/satoshun/example/sample/MainActivity.kt
+++ app/src/main/java/com/github/satoshun/example/sample/MainActivity.kt
@@ -2,11 +2,39 @@ package com.github.satoshun.example.sample
 
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
 import com.github.satoshun.example.sample.databinding.MainActBinding
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class MainActivity : BaseActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     val binding = DataBindingUtil.setContentView<MainActBinding>(this, R.layout.main_act)
+
+    val childJob = launch { }
+    addJob(childJob)
+  }
+
+  override fun onResume() {
+    super.onResume()
+    val childJob = launch { }
+    addJob(childJob)
+  }
+}
+
+class AnyObserver(
+  private val job: Job,
+  private val event: Lifecycle.Event
+) : LifecycleObserver {
+  @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+  fun onEvent(owner: LifecycleOwner, event: Lifecycle.Event) {
+    if (event == this.event) {
+      owner.lifecycle.removeObserver(this)
+      job.cancel()
+    }
   }
 }
