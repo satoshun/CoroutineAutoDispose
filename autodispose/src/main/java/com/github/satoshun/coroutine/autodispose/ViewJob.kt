4009fb066c90934323c574216f5f3715712aa4fc
SatoShun <shun.sato1@gmail.com>
2018-12-23 09:50:19 +0000

    add addJob for View

---
+++ autodispose/src/main/java/com/github/satoshun/coroutine/autodispose/ViewJob.kt
@@ -0,0 +1,32 @@
+package com.github.satoshun.coroutine.autodispose
+
+import android.view.View
+import com.github.satoshun.coroutine.autodispose.lifecycle.ExperimentalApi
+import kotlinx.coroutines.CompletionHandler
+import kotlinx.coroutines.Job
+
+@ExperimentalApi(message = "There is a possibility that it will change")
+fun View.addJob(job: Job) {
+  val listener = ViewListener(this, job)
+  this.addOnAttachStateChangeListener(listener)
+}
+
+private class ViewListener(
+  private val view: View,
+  private val job: Job
+) : View.OnAttachStateChangeListener,
+  CompletionHandler {
+  override fun onViewDetachedFromWindow(v: View) {
+    view.removeOnAttachStateChangeListener(this)
+    job.cancel()
+  }
+  
+  override fun onViewAttachedToWindow(v: View) {
+    // do nothing
+  }
+
+  override fun invoke(cause: Throwable?) {
+    view.removeOnAttachStateChangeListener(this)
+    job.cancel()
+  }
+}
---
+++ autodispose/src/test/java/com/github/satoshun/coroutine/autodispose/ViewJobTest.kt
@@ -0,0 +1,61 @@
+package com.github.satoshun.coroutine.autodispose
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.satoshun.coroutine.autodispose.lifecycle.JobSubject
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ViewJobTest {
+  @Test
+  fun addJob_detached() {
+    val job = GlobalScope.launch { delay(10000) }
+    val scenario = ActivityScenario.launch(ComponentActivity::class.java)
+
+    var view: View? = null
+    scenario.moveToState(Lifecycle.State.CREATED)
+    scenario.onActivity {
+      view = TextView(it)
+      it.setContentView(view)
+
+      view!!.addJob(job)
+    }
+    JobSubject.assertThat(job).isNotCanceled()
+
+    scenario.onActivity {
+      // detach view
+      it.findViewById<ViewGroup>(android.R.id.content).removeView(view)
+    }
+    JobSubject.assertThat(job).isCanceled()
+  }
+
+  @Test
+  fun addJob_cancel() {
+    val job = GlobalScope.launch { delay(10000) }
+    val scenario = ActivityScenario.launch(ComponentActivity::class.java)
+
+    var view: View? = null
+    scenario.moveToState(Lifecycle.State.CREATED)
+    scenario.onActivity {
+      view = TextView(it)
+      it.setContentView(view)
+
+      view!!.addJob(job)
+    }
+    JobSubject.assertThat(job).isNotCanceled()
+
+    job.cancel()
+    JobSubject.assertThat(job).isCanceled()
+
+    // todo check is called View.removeOnAttachStateChangeListener
+  }
+}
