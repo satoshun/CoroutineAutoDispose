777cc811e12ce6212c1dc36e0340081787957cc9
SatoShun <shun.sato1@gmail.com>
2018-12-23 03:37:27 +0000

    add test

---
+++ autodispose/src/test/java/com/github/satoshun/coroutine/autodispose/lifecycle/LifecycleOwnerJobTest.kt
@@ -0,0 +1,53 @@
+package com.github.satoshun.coroutine.autodispose.lifecycle
+
+import androidx.activity.ComponentActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleRegistry
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class LifecycleOwnerJobTest {
+  @Test
+  fun addJob_onCreated() {
+    val job = GlobalScope.launch { delay(100000) }
+
+    val scenario = ActivityScenario.launch(ComponentActivity::class.java)
+
+    scenario.moveToState(Lifecycle.State.CREATED)
+    scenario.onActivity {
+      it.addJob(job)
+    }
+    assertThat(job.isCancelled).isFalse()
+
+    scenario.moveToState(Lifecycle.State.RESUMED)
+    assertThat(job.isCancelled).isFalse()
+
+    scenario.moveToState(Lifecycle.State.DESTROYED)
+    assertThat(job.isCancelled).isTrue()
+  }
+
+  @Test
+  fun addJob_onResumed() {
+    val job = GlobalScope.launch { delay(100000) }
+
+    val scenario = ActivityScenario.launch(ComponentActivity::class.java)
+
+    scenario.moveToState(Lifecycle.State.RESUMED)
+    scenario.onActivity {
+      it.addJob(job)
+    }
+    assertThat(job.isCancelled).isFalse()
+
+    scenario.onActivity {
+      (it.lifecycle as LifecycleRegistry).handleLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    }
+    assertThat(job.isCancelled).isTrue()
+  }
+}
--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -44,10 +44,11 @@ object Libs {
 
   const val junit = "junit:junit:4.12"
   const val truth = "com.google.truth:truth:0.42"
-  const val test_rules = "androidx.test:rules:1.1.0"
-  const val test_runner = "androidx.test:runner:1.1.0"
+  const val test_rules = "androidx.test:rules:1.1.1"
+  const val test_runner = "androidx.test:runner:1.1.1"
+  const val test_core = "androidx.test:core-ktx:1.1.0"
   const val test_junit_rules = "androidx.test.ext:junit:1.0.0"
   const val mockito_kotlin = "com.nhaarman:mockito-kotlin-kt1.1:1.5.0"
   const val espresso = "androidx.test.espresso:espresso-core:3.1.0"
-  const val robolectric = "org.robolectric:robolectric:4.0"
+  const val robolectric = "org.robolectric:robolectric:4.1"
 }
