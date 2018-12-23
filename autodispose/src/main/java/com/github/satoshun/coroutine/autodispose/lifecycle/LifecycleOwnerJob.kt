664e7843290abc49c55fbed24d7ff49e89f7c7a9
SatoShun <shun.sato1@gmail.com>
2018-12-23 04:00:12 +0000

    add CompletionHandler

--- autodispose/src/main/java/com/github/satoshun/coroutine/autodispose/lifecycle/LifecycleOwnerJob.kt
+++ autodispose/src/main/java/com/github/satoshun/coroutine/autodispose/lifecycle/LifecycleOwnerJob.kt
@@ -4,29 +4,37 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.OnLifecycleEvent
+import kotlinx.coroutines.CompletionHandler
 import kotlinx.coroutines.Job
 
 fun LifecycleOwner.addJob(job: Job) {
   val state = lifecycle.currentState
   val event = when (state) {
-    Lifecycle.State.DESTROYED -> Lifecycle.Event.ON_DESTROY // ON_DESTROY?
+    Lifecycle.State.DESTROYED -> Lifecycle.Event.ON_DESTROY
     Lifecycle.State.INITIALIZED -> Lifecycle.Event.ON_DESTROY
     Lifecycle.State.CREATED -> Lifecycle.Event.ON_DESTROY
     Lifecycle.State.STARTED -> Lifecycle.Event.ON_STOP
     Lifecycle.State.RESUMED -> Lifecycle.Event.ON_PAUSE
   }
-  lifecycle.addObserver(AnyObserver(job, event))
+  val observer = AnyObserver(job, event, lifecycle)
+  lifecycle.addObserver(observer)
+  job.invokeOnCompletion(observer)
 }
 
 private class AnyObserver(
   private val job: Job,
-  private val event: Lifecycle.Event
-) : LifecycleObserver {
+  private val event: Lifecycle.Event,
+  private val lifecycle: Lifecycle
+) : LifecycleObserver, CompletionHandler {
   @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
   fun onEvent(owner: LifecycleOwner, event: Lifecycle.Event) {
     if (event == this.event) {
       owner.lifecycle.removeObserver(this)
       job.cancel()
     }
+  }
+
+  override fun invoke(cause: Throwable?) {
+    lifecycle.removeObserver(this)
   }
 }
--- autodispose/src/test/java/com/github/satoshun/coroutine/autodispose/lifecycle/LifecycleOwnerJobTest.kt
+++ autodispose/src/test/java/com/github/satoshun/coroutine/autodispose/lifecycle/LifecycleOwnerJobTest.kt
@@ -7,8 +7,10 @@ import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -49,5 +51,26 @@ class LifecycleOwnerJobTest {
       (it.lifecycle as LifecycleRegistry).handleLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     }
     assertThat(job.isCancelled).isTrue()
+  }
+
+  @Test
+  fun completionHandler() {
+    val fixedObserverSize = 1
+    val job = GlobalScope.launch { delay(100000) }
+
+    val scenario = ActivityScenario.launch(ComponentActivity::class.java)
+    scenario.moveToState(Lifecycle.State.CREATED)
+    scenario.onActivity { it.addJob(job) }
+
+    scenario.onActivity {
+      assertThat((it.lifecycle as LifecycleRegistry).observerCount)
+        .isEqualTo(1 + fixedObserverSize)
+    }
+
+    runBlocking { job.cancelAndJoin() }
+    scenario.onActivity {
+      assertThat((it.lifecycle as LifecycleRegistry).observerCount)
+        .isEqualTo(0 + fixedObserverSize)
+    }
   }
 }
