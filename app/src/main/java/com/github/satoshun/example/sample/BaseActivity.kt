6b575a9e577c85d2a343a486b7ec2fe05fc80f92
SatoShun <shun.sato1@gmail.com>
2018-12-23 02:48:46 +0000

    use ContinuationInterceptor approach

--- app/src/main/java/com/github/satoshun/example/sample/BaseActivity.kt
+++ app/src/main/java/com/github/satoshun/example/sample/BaseActivity.kt
@@ -1,20 +1,27 @@
 package com.github.satoshun.example.sample
 
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.OnLifecycleEvent
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.Runnable
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.ContinuationInterceptor
 import kotlin.coroutines.CoroutineContext
 
 abstract class BaseActivity : AppCompatActivity(),
   CoroutineScope {
 
   private val job = Job()
-  override val coroutineContext: CoroutineContext get() = job + Dispatchers.Main
+  override val coroutineContext: CoroutineContext get() = job + Dispatchers.Main + LifecycleContinuationInterceptor(this)
 
   override fun onDestroy() {
     super.onDestroy()
@@ -43,5 +50,35 @@ private class AnyObserver(
       owner.lifecycle.removeObserver(this)
       job.cancel()
     }
+  }
+}
+
+class LifecycleContinuationInterceptor(
+  private val lifecycleOwner: LifecycleOwner
+) : ContinuationInterceptor {
+  override val key: CoroutineContext.Key<*>
+    get() = ContinuationInterceptor
+
+  override fun <T> interceptContinuation(continuation: Continuation<T>): Continuation<T> {
+    Log.d("hoge3", continuation.context[Job].toString())
+    // todo nullable?
+    lifecycleOwner.addJob(continuation.context[Job]!!)
+    return continuation
+  }
+}
+
+// todo more delegate and extends MainCoroutineDispatcher?
+class WrapperDispatcher(
+  private val original: CoroutineDispatcher
+) : CoroutineDispatcher() {
+  override fun dispatch(context: CoroutineContext, block: Runnable) =
+    original.dispatch(context, block)
+
+  @ExperimentalCoroutinesApi override fun isDispatchNeeded(context: CoroutineContext): Boolean {
+    return original.isDispatchNeeded(context)
+  }
+
+  @InternalCoroutinesApi override fun dispatchYield(context: CoroutineContext, block: Runnable) {
+    original.dispatchYield(context, block)
   }
 }
--- app/src/main/java/com/github/satoshun/example/sample/MainActivity.kt
+++ app/src/main/java/com/github/satoshun/example/sample/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.github.satoshun.example.sample
 
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.github.satoshun.example.sample.databinding.MainActBinding
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : BaseActivity() {
@@ -10,13 +13,32 @@ class MainActivity : BaseActivity() {
     super.onCreate(savedInstanceState)
     val binding = DataBindingUtil.setContentView<MainActBinding>(this, R.layout.main_act)
 
-    val childJob = launch { }
-    addJob(childJob)
+    val childJob = launch {
+      Log.d("hoge", coroutineContext[Job].toString())
+      while (true) {
+        delay(1200)
+        Log.d("hoge", "delayed")
+      }
+    }
+    childJob.invokeOnCompletion {
+      Log.d("hoge", "completed")
+    }
+//    addJob(childJob)
   }
 
   override fun onResume() {
     super.onResume()
-    val childJob = launch { }
-    addJob(childJob)
+    val childJob = launch {
+      Log.d("hoge2", coroutineContext[Job].toString())
+      while (true) {
+        delay(3000)
+        Log.d("hoge2", "delayed")
+      }
+    }
+    childJob.invokeOnCompletion {
+      Log.d("hoge2", "completed")
+    }
+
+//    addJob(childJob)
   }
 }
