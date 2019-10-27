8ecf22e1ce33a1bf5f915f29c98fbac29dc3b39b
SatoShun <shun.sato1@gmail.com>
2019-10-27 06:42:00 +0000

    update some androidx libs for test and sample project

--- buildSrc/src/main/kotlin/dependencies/Dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies/Dependencies.kt
@@ -16,14 +16,14 @@ const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib:$VKOTLIN"
 const val COROUTINE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$VCOUROUTINE"
 const val UI_COROUTINE = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$VCOUROUTINE"
 
-const val KTX = "androidx.core:core-ktx:1.0.0"
+const val KTX = "androidx.core:core-ktx:1.1.0"
 
-const val APPCOMPAT = "androidx.appcompat:appcompat:1.0.2"
-const val CONSTRAINTLAYOUT = "androidx.constraintlayout:constraintlayout:2.0.0-alpha4"
-const val RECYCLER_VIEW = "androidx.recyclerview:recyclerview:1.1.0-alpha04"
+const val APPCOMPAT = "androidx.appcompat:appcompat:1.1.0"
+const val CONSTRAINTLAYOUT = "androidx.constraintlayout:constraintlayout:2.0.0-beta3"
+const val RECYCLER_VIEW = "androidx.recyclerview:recyclerview:1.0.0"
 
-const val ACTIVITYX = "androidx.activity:activity-ktx:1.0.0-alpha06"
-const val FRAGMENTX = "androidx.fragment:fragment-ktx:1.1.0-alpha06"
+const val ACTIVITYX = "androidx.activity:activity-ktx:1.0.0"
+const val FRAGMENTX = "androidx.fragment:fragment-ktx:1.1.0"
 
 const val VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.0.0"
 const val LIVEDATA = "androidx.lifecycle:lifecycle-livedata:2.0.0"
