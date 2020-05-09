5eb4a03253f3cb9d338d1d8050dc1c545820e885
SatoShun <shun.sato1@gmail.com>
2020-05-09 23:48:36 +0000

    Update Coroutine 1.3.6

--- buildSrc/src/main/kotlin/dependencies/Dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies/Dependencies.kt
@@ -10,7 +10,7 @@ const val ANDROID_PLUGIN = "com.android.tools.build:gradle:3.5.3"
 const val KOTLIN_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:$VKOTLIN"
 const val PUBLISH_PLUGIN = "com.vanniktech:gradle-maven-publish-plugin:0.8.0"
 
-private const val VCOUROUTINE = "1.3.2"
+private const val VCOUROUTINE = "1.3.6"
 const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib:$VKOTLIN"
 const val COROUTINE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$VCOUROUTINE"
 const val UI_COROUTINE = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$VCOUROUTINE"
