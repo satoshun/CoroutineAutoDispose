037a9a0f91cc0c17f07158a7cb9bef9388102187
SatoShun <shun.sato1@gmail.com>
2021-05-29 06:20:44 +0000

    coroutine 1.4.3, kotlin 1.4.32

--- buildSrc/src/main/kotlin/dependencies/Dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies/Dependencies.kt
@@ -4,13 +4,13 @@ const val COMPILE_SDK = 30
 const val MIN_SDK = 14
 const val TARGET_SDK = 30
 
-const val VKOTLIN = "1.3.72"
+const val VKOTLIN = "1.4.32"
 
 const val ANDROID_PLUGIN = "com.android.tools.build:gradle:4.2.1"
 const val KOTLIN_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:$VKOTLIN"
 const val PUBLISH_PLUGIN = "com.vanniktech:gradle-maven-publish-plugin:0.15.1"
 
-private const val VCOUROUTINE = "1.3.6"
+private const val VCOUROUTINE = "1.4.3"
 const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib:$VKOTLIN"
 const val COROUTINE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$VCOUROUTINE"
 const val UI_COROUTINE = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$VCOUROUTINE"
